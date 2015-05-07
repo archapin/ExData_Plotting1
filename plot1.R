79c6948f2148fafada0939cc4cbaa55d13fa0ce7
archapin <archapin@gmail.com>
2015-05-06 21:22:25 -0600

    Plot1 files

---
+++ plot1.R
@@ -0,0 +1,26 @@
+
+# LOADING THE DATA
+# _________________
+
+# Getting the names of the columns from the original file
+
+
+datacolnames<- as.character(read.table("household_power_consumption.txt",
+                          sep=";", nrows=1, header=FALSE, 
+                          stringsAsFactors = FALSE))
+
+#  Reading the lines with the information for dates 2007-02-01 to 2007-02-02
+
+powerconsumption <- read.table("household_power_consumption.txt",
+                         sep = ";", skip = 66636, nrows=2880,
+                         colClasses = c("character","character", 
+                        rep("numeric",7)), col.names = datacolnames, 
+                        na.strings = "?")
+
+
+# Making the plot
+# 
+
+
+hist(powerconsumption$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
